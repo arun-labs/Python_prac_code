b4b37e0bf8bcb2d3e6bb0d2194763a732cd5b504
arun-labs <aruningole1921@gmail.com>
2023-11-13 14:53:14 +0530

    this is file commit

---
+++ File_IO/vs.py
@@ -0,0 +1,13 @@
+# f = open('file.txt','r')
+# print(f)
+# text = f.read()
+# print(text)
+# f.close()
+
+# z = open('file2.txt','w')
+# z.write('kjgrekjgnrlvm')
+# z.close()
+
+k = open('file3.txt','a')
+k.write('dfkvnefgnrkgnr')
+k.close()
